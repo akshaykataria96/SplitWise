380ffefb2112741e97c4f5175ff5b0075895621d
akshay.vk96@gmail.com <akshay.vk96@gmail.com>
2019-05-26 14:23:50 +0530

    Initial Commit

---
+++ GaanaChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GaanaChallenge
+//
+//  Created by Akshay Kataria on 26/05/19.
+//  Copyright © 2019 Akshay Kataria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
